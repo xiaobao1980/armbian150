800a07595633e72d888a8495280a3318645452cc
Oleg Ivanov <balbes-150@yandex.ru>
2022-06-25 15:49:11 +0300

    20220625

---
+++ config/desktop/jammy/environments/icewm/armbian/create_desktop_package.sh
@@ -0,0 +1,18 @@
+# install lightdm greeter
+cp -R "${SRC}"/packages/blobs/desktop/lightdm "${destination}"/etc/armbian
+
+# install default desktop settings
+mkdir -p "${destination}"/etc/skel
+cp -R "${SRC}"/packages/blobs/desktop/skel/. "${destination}"/etc/skel
+
+# install logo for login screen
+mkdir -p "${destination}"/usr/share/pixmaps/armbian
+cp "${SRC}"/packages/blobs/desktop/icons/armbian.png "${destination}"/usr/share/pixmaps/armbian
+
+# install desktop wallpapers
+mkdir -p "${destination}"/usr/share/backgrounds/armbian/
+cp "${SRC}"/packages/blobs/desktop/desktop-wallpapers/armbian*.jpg "${destination}"/usr/share/backgrounds/armbian/
+
+# install lightdm wallpapers
+mkdir -p "${destination}"/usr/share/backgrounds/armbian-lightdm/
+cp "${SRC}"/packages/blobs/desktop/lightdm-wallpapers/*.jpg "${destination}"/usr/share/backgrounds/armbian-lightdm

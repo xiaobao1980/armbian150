37a107796cdbbdfc6947280d2731a9582ac66565
balbes150 <balbes-150@yandex.ru>
2022-07-20 13:16:07 +0300

    fix

--- extensions/grub-n.sh
+++ extensions/grub-n.sh
@@ -1,5 +1,5 @@
 # This runs *after* user_config. Don't change anything not coming from other variables or meant to be configured by the user.
-function extension_prepare_config__prepare_flash_kernel() {
+function extension_prepare_config__prepare_grub-n() {
 	display_alert "Prepare config" "${EXTENSION}" "info"
 	# Extension configuration defaults.
 	export DISTRO_GENERIC_KERNEL=${DISTRO_GENERIC_KERNEL:-no}                    # if yes, does not build our own kernel, instead, uses generic one from distro
@@ -7,7 +7,7 @@ function extension_prepare_config__prepare_flash_kernel() {
 	export UEFI_GRUB_DISABLE_OS_PROBER="${UEFI_GRUB_DISABLE_OS_PROBER:-}"        # 'true' will disable os-probing, useful for SD cards.
 	export UEFI_GRUB_DISTRO_NAME="${UEFI_GRUB_DISTRO_NAME:-Armbian}"             # Will be used on grub menu display
 	export UEFI_GRUB_TIMEOUT=${UEFI_GRUB_TIMEOUT:-0}                             # Small timeout by default
-	export UEFI_ENABLE_BIOS_AMD64="${UEFI_ENABLE_BIOS_AMD64:-yes}"               # Enable BIOS too if target is amd64
+	export UEFI_ENABLE_BIOS_AMD64="${UEFI_ENABLE_BIOS_AMD64:-no}"               # Enable BIOS too if target is amd64
 	export UEFI_EXPORT_KERNEL_INITRD="${UEFI_EXPORT_KERNEL_INITRD:-no}"          # Export kernel and initrd for direct kernel boot "kexec"
 	# User config overrides.
 #	export BOOTCONFIG="${BOTCONFIG:-none}"                                                     # To try and convince lib/ to not build or install u-boot.
@@ -21,71 +21,39 @@ function extension_prepare_config__prepare_flash_kernel() {
 	local uefi_packages="efibootmgr efivar cloud-initramfs-growroot"             # Use growroot, add some efi-related packages
 	uefi_packages="os-prober grub-efi-${ARCH}-bin ${uefi_packages}"              # This works for Ubuntu and Debian, by sheer luck; common for EFI and BIOS
 
-	# BIOS-compatibility for amd64
-	if [[ "${ARCH}" == "amd64" ]]; then
-		export UEFI_GRUB_TARGET="x86_64-efi" # Default for x86_64
-		if [[ "${UEFI_ENABLE_BIOS_AMD64}" == "yes" ]]; then
-			export uefi_packages="${uefi_packages} grub-pc-bin grub-pc"
-			export UEFI_GRUB_TARGET_BIOS="i386-pc"
-			export BIOSSIZE=4 # 4 MiB BIOS partition
-		else
-			export uefi_packages="${uefi_packages} grub-efi-${ARCH}"
-		fi
-	fi
+#	if [[ "${DISTRIBUTION}" == "Ubuntu" ]]; then
+#	display_alert "Prepare config Ubuntu" "${EXTENSION}" "info"
 
 	[[ "${ARCH}" == "arm64" ]] && export uefi_packages="${uefi_packages} grub-efi-${ARCH}"
 	[[ "${ARCH}" == "arm64" ]] && export UEFI_GRUB_TARGET="arm64-efi" # Default for arm64-efi
 
-	if [[ "${DISTRIBUTION}" == "Ubuntu" ]]; then
-		DISTRO_KERNEL_PACKAGES="linux-image-generic"
-		DISTRO_FIRMWARE_PACKAGES="linux-firmware"
-	elif [[ "${DISTRIBUTION}" == "Debian" ]]; then
-		DISTRO_KERNEL_PACKAGES="linux-image-${ARCH}"
-		DISTRO_FIRMWARE_PACKAGES="firmware-linux-free"
-		# Debian's prebuilt kernels dont support hvc0, hack.
-		if [[ "${SERIALCON}" == "hvc0" ]]; then
-			display_alert "Debian's kernels don't support hvc0, changing to ttyS0" "${DISTRIBUTION}" "wrn"
-			export SERIALCON="ttyS0"
-		fi
-	fi
+#	if [[ "${DISTRIBUTION}" == "Debian" ]]; then
+#	display_alert "Prepare Debian" "${EXTENSION}" "info"
 
-	if [[ "${DISTRO_GENERIC_KERNEL}" == "yes" ]]; then
-		export VER="generic"
-		unset KERNELSOURCE                 # This should make Armbian skip most stuff. At least, I hacked it to.
-		export INSTALL_ARMBIAN_FIRMWARE=no # Should skip build and install of Armbian-firmware.
-	else
-#		export KERNELDIR="linux-uefi-${LINUXFAMILY}" # Avoid sharing a source tree with others, until we know it's safe.
-		# Don't install anything. Armbian handles everything.
-		DISTRO_KERNEL_PACKAGES=""
-		DISTRO_FIRMWARE_PACKAGES=""
-	fi
+#	local uefi_packages=""
 
-	export PACKAGE_LIST_BOARD="${PACKAGE_LIST_BOARD} ${DISTRO_FIRMWARE_PACKAGES} ${DISTRO_KERNEL_PACKAGES}  ${uefi_packages}"
+#		# Debian's prebuilt kernels dont support hvc0, hack.
+#		if [[ "${SERIALCON}" == "hvc0" ]]; then
+#			display_alert "Debian's kernels don't support hvc0, changing to ttyS0" "${DISTRIBUTION}" "wrn"
+#			export SERIALCON="ttyS0"
+#		fi
+#	fi
+
+	DISTRO_KERNEL_PACKAGES=""
+	DISTRO_FIRMWARE_PACKAGES=""
+
+	export PACKAGE_LIST_BOARD="${PACKAGE_LIST_BOARD} ${DISTRO_FIRMWARE_PACKAGES} ${DISTRO_KERNEL_PACKAGES} ${uefi_packages}"
 
 	display_alert "Activating" "GRUB with SERIALCON=${SERIALCON}; timeout ${UEFI_GRUB_TIMEOUT}; BIOS=${UEFI_GRUB_TARGET_BIOS}" ""
 }
 
-# @TODO: extract u-boot into an extension, so that core bsps don't have this stuff in there to begin with.
-# @TODO: this code is duplicated in flash-kernel.sh extension, so another reason to refactor the root of the evil
-#post_family_tweaks_bsp__remove_uboot_grub() {
-#	display_alert "Removing uboot from BSP" "${EXTENSION}" "info"
-	# Simply remove everything with 'uboot' or 'u-boot' in their filenames from the BSP package.
-	# shellcheck disable=SC2154 # $destination is the target dir of the bsp building function
-#	find "$destination" -type f | grep -e "uboot" -e "u-boot" | xargs rm
-#}
-
-#pre_umount_final_image__remove_uboot_initramfs_hook_grub() {
-	# even if BSP still contained this (cached .deb), make sure by removing from ${MOUNT}
-#	[[ -f "$MOUNT"/etc/initramfs/post-update.d/99-uboot ]] && rm -v "$MOUNT"/etc/initramfs/post-update.d/99-uboot
-#}
-
 pre_umount_final_image__install_grub() {
+
 	configure_grub
 	local chroot_target=$MOUNT
 	display_alert "Installing bootloader" "GRUB" "info"
 
 	# getting rid of the dtb package, if installed, is hard. for now just zap it, otherwise update-grub goes bananas
-#	rm -rf "$MOUNT"/boot/dtb* || true
 	cp -r "$MOUNT"/boot/dtb/rockchip "$MOUNT"/boot/efi/
 	cp -r "$MOUNT"/boot/dtb/nvidia "$MOUNT"/boot/efi/
 
@@ -96,13 +64,6 @@ pre_umount_final_image__install_grub() {
 
 	# Mount the chroot...
 	mount_chroot "$chroot_target/" # this already handles /boot/efi which is required for it to work.
-
-	if [[ "${UEFI_GRUB_TARGET_BIOS}" != "" ]]; then
-		display_alert "Installing GRUB BIOS..." "${UEFI_GRUB_TARGET_BIOS} device ${LOOP}" ""
-		chroot "$chroot_target" /bin/bash -c "grub-install --verbose --target=${UEFI_GRUB_TARGET_BIOS} ${LOOP}" >>"$DEST"/"${LOG_SUBPATH}"/install.log 2>&1 || {
-			exit_with_error "${install_grub_cmdline} failed!"
-		}
-	fi
 
 	local install_grub_cmdline="update-initramfs -c -k all && update-grub && grub-install --verbose --target=${UEFI_GRUB_TARGET} --no-nvram --removable" # nvram is global to the host, even across chroot. take care.
 	display_alert "Installing GRUB EFI..." "${UEFI_GRUB_TARGET}" ""
@@ -122,6 +83,10 @@ pre_umount_final_image__install_grub() {
 		set prefix=(\$root)'/boot/grub'
 		configfile \$prefix/grub.cfg
 	grubEfiCfg
+
+	if [[ $BOARD == jetson-nano ]]; then
+		cp -R $SRC/packages/blobs/jetson/boot.scr "${MOUNT}"/boot/efi/boot.scr
+	fi
 
 	umount_chroot "$chroot_target/"
 
@@ -163,4 +128,9 @@ configure_grub() {
 			GRUB_TERMINAL="${UEFI_GRUB_TERMINAL}"
 		grubCfgFragTerminal
 	fi
+
+#	if [[ "a${BOOT_FDT_FILE}" != "a" ]]; then
+#		cat <<-grubCfgFrag >>"${MOUNT}"/etc/default/grub.d/98-armbian.cfg
+#		BOOT_FDT_FILE="nvidia/tegra210-p3450-0000.dtb"
+#	fi
 }

c52ba78a3d0c765ee9a3f554263873c9bf0c6c12
balbes150 <balbes-150@yandex.ru>
2022-11-19 17:48:40 +0300

    20221119

--- lib/functions/rootfs/distro-specific.sh
+++ lib/functions/rootfs/distro-specific.sh
@@ -49,13 +49,17 @@ install_distribution_specific() {
 	# configure language and locales
 	display_alert "Configuring locales" "$DEST_LANG" "info"
 	if [[ -f $SDCARD/etc/locale.gen ]]; then
-		[ -n "$DEST_LANG" ] && sed -i "s/^# $DEST_LANG/$DEST_LANG/" $SDCARD/etc/locale.gen
+		if [ -n "$DEST_LANG" ]; then 
+#&& sed -i "s/^# $DEST_LANG/$DEST_LANG/" $SDCARD/etc/locale.gen
+	display_alert "Configuring locales GEN 1 *****" "$DEST_LANG" "info"
+		sed -i 's/^# $DEST_LANG/$DEST_LANG/' $SDCARD/etc/locale.gen
 		sed -i '/ C.UTF-8/s/^# //g' $SDCARD/etc/locale.gen
 		sed -i '/en_US.UTF-8/s/^# //g' $SDCARD/etc/locale.gen
+		fi
 	fi
-	eval 'LC_ALL=C LANG=C chroot $SDCARD /bin/bash -c "locale-gen"' ${OUTPUT_VERYSILENT:+' >/dev/null 2>/dev/null'}
-	[ -n "$DEST_LANG" ] && eval 'LC_ALL=C LANG=C chroot $SDCARD /bin/bash -c \
-	"update-locale --reset LANG=$DEST_LANG LANGUAGE=$DEST_LANG LC_ALL=$DEST_LANG"' ${OUTPUT_VERYSILENT:+' >/dev/null 2>/dev/null'}
+#	eval 'LC_ALL=C LANG=C chroot $SDCARD /bin/bash -c "locale-gen"' ${OUTPUT_VERYSILENT:+' >/dev/null 2>/dev/null'}
+#	[ -n "$DEST_LANG" ] && eval 'LC_ALL=C LANG=C chroot $SDCARD /bin/bash -c \
+#	"update-locale --reset LANG=$DEST_LANG LANGUAGE=$DEST_LANG LC_ALL=$DEST_LANG"' ${OUTPUT_VERYSILENT:+' >/dev/null 2>/dev/null'}
 
 	# Basic Netplan config. Let NetworkManager/networkd manage all devices on this system
 	[[ -d "${SDCARD}"/etc/netplan ]] && cat <<- EOF > "${SDCARD}"/etc/netplan/armbian-default.yaml

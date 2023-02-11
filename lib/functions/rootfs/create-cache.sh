fa2efbbe5dc95b247286d54e0fd596a7f4084f7a
balbes150 <balbes-150@yandex.ru>
2023-02-11 11:27:58 +0300

    fix

--- lib/functions/rootfs/create-cache.sh
+++ lib/functions/rootfs/create-cache.sh
@@ -80,7 +80,7 @@ create_rootfs_cache() {
 	else
 
 #		local ROOT_FS_CREATE_VERSION=${ROOT_FS_CREATE_VERSION:-$(date --utc +"%Y%m%d")}
-		local ROOT_FS_CREATE_VERSION=${ROOT_FS_CREATE_VERSION:-$(packages_hash)}
+		local ROOT_FS_CREATE_VERSION="0121"
 		local cache_name=${ARCH}-${RELEASE}-${cache_type}-${packages_hash}-${ROOT_FS_CREATE_VERSION}.tar.zst
 		local cache_fname=${SRC}/cache/rootfs/${cache_name}
 

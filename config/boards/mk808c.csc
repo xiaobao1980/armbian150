eb7513407ee797b006c5917f4c931d2ed70e0a6b
balbes150 <balbes-150@yandex.ru>
2023-04-11 17:31:27 +0300

    init

--- config/boards/mk808c.csc
+++
@@ -1,6 +0,0 @@
-# Allwinner A20 dual core 1Gb SoC
-BOARD_NAME="MK808C"
-BOARDFAMILY="sun7i"
-HAS_VIDEO_OUTPUT="no"
-BOOTCONFIG="MK808C_defconfig"
-KERNEL_TARGET="legacy,current,edge"
--- lib/logging/runners.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-run_on_sdcard() {
-
-	# Lack of quotes allows for redirections and pipes easily.
-	chroot "${SDCARD}" /bin/bash -c "${@}" >> "${DEST}"/${LOG_SUBPATH}/install.log
-
-}
